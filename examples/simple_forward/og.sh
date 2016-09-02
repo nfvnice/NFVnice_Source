bd2c46c9c7325cb7555636e74857ebf0d2ee1343
sameergk <root@flashstack-3.cs.ucr.edu>
2016-09-01 17:55:06 -0700

    adding hlper scripts

---
+++ examples/simple_forward/og.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cpu=$1
+service=$2
+dst=$3
+inst_id=$4
+print=$5
+
+if [ -z $dst ]
+then
+        echo "$0 [cpu-list] [Service ID] [DST] [PRINT]"
+        echo "$0 3,7,9 1 2 --> cores 3,7, and 9, with Service ID 1, and forwards to service ID 2"
+        echo "$0 3,7,9 1 2 1000 --> cores 3,7, and 9, with Service ID 1, forwards to service ID 2,  and Print Rate of 1000"
+        exit 1
+fi
+
+if [ -z $inst_id ]
+then
+inst_id=7
+fi
+if [ -z $print ]
+then
+        sudo ./build/forward -l $cpu -n 3 --proc-type=secondary -- -r $service -n $inst_id -- -d $dst
+else
+        sudo ./build/forward -l $cpu -n 3 --proc-type=secondary -- -r $service -n $inst_id -- -d $dst -p $print
+fi
