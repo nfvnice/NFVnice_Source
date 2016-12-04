bde2ff5566a7b619e27e4588e84ba9b9ebbd4e72
sameergk <root@flashstack-3.cs.ucr.edu>
2016-12-03 19:30:33 -0800

    missc update

--- examples/pkt_logger/pkt_logger.c
+++ examples/pkt_logger/pkt_logger.c
@@ -447,8 +447,8 @@ int write_log_buffer(pkt_buf_t *pbuf) {
         pbuf->req_status = aio_write(pbuf->aiocb);
 #ifdef USE_SYNC_IO
         ret = pwrite(globals.fd, pbuf->buf, pbuf->aiocb->aio_nbytes, pbuf->aiocb->aio_offset);
-        pbuf->state = BUF_FREE;
         globals.cur_buf_index = (((globals.cur_buf_index+1) % (globals.max_bufs))? (globals.cur_buf_index+1):(0));
+        refresh_log_buffer(pbuf);
         return ret;
 #endif
         if(-1 == pbuf->req_status) {
